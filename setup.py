405b744824c28d3f95d24a2589a9551d84dc2a08
Omid <omid.bakhshi.dev@gmail.com>
2025-10-27 18:34:14 +0330

    fix: Correct README filename case in setup.py
    - Change 'README.md' to 'readme.md' to match actual filename
    - Fixes GitHub Actions CI build failure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Location: setup.py (root directory)
 
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
